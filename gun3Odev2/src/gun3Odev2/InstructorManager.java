a29cdf438b30fa79370d45cfe42df1780e505e99
glbyz <32942947+glbyz@users.noreply.github.com>
2021-04-30 23:36:03 +0300

    Edit

--- gun3Odev2/src/gun3Odev2/InstructorManager.java
+++ gun3Odev2/src/gun3Odev2/InstructorManager.java
@@ -5,16 +5,16 @@ public class InstructorManager extends UserManager {
 	@Override
 	public void add(User user) {
 		
-		System.out.println(user.firstName+ "  "+ user.lastName+ " " + " adlý eðitmen usereklendi");
+		System.out.println(user.firstName+ "  "+ user.lastName+ " " + " adli egitmen eklendi");
 	}
 	
 	public void update(Instructor instructor ) {
-		System.out.println(instructor.instructorNumber + " " + "numaralý Eðitmen "+ 
+		System.out.println(instructor.instructorNumber + " " + "numarali Egitmen "+ 
 	instructor.firstName+ " " +  " "+ instructor.lastName + " Güncellendi" );
 	}
 	
 	@Override
 	public void delete(User user) {
-		System.out.println(user.firstName+ user.lastName+" "+  " adlý eðitmen silindi");
+		System.out.println(user.firstName+ user.lastName+" "+  " adli Egitmen silindi");
 	}
 }
--- gun3Odev2/src/gun3Odev2/StudentManager.java
+++ gun3Odev2/src/gun3Odev2/StudentManager.java
@@ -4,16 +4,16 @@ public class StudentManager extends UserManager{
 
 	@Override
 	public void add(User user) {
-		System.out.println(user.firstName+ " " + user.lastName+" "+ " adlý öðrenci eklendi");
+		System.out.println(user.firstName+ " " + user.lastName+" "+ " adli ogrenci eklendi");
 	}
 	
 	@Override
 	public void delete(User user) {
-		System.out.println(user.firstName+ " " +  user.lastName+" "+ " adlý öðrenci silindi");
+		System.out.println(user.firstName+ " " +  user.lastName+" "+ " adli ogrenci silindi");
 	}
 	
 	@Override
 	public void update(User user) {
-		System.out.println(user.firstName+ " " + user.getFirstName() +" "+  " adlý öðrenci güncellendi");
+		System.out.println(user.firstName+ " " + user.getFirstName() +" "+  " adli ogrenci guncellendi");
 	}
 }
